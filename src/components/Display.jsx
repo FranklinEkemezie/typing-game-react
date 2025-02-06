0664640e66fd08d1e255e033d307bd79cba969f3
Shadow Walker <franklynpeter2006@gmail.com>
2025-02-06 01:33:53 +0100

    add typewriter display to codebase

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,74 +1,95 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect} from 'react';
 import displayStyles from '../assets/styles/display.module.css';
-import {TEXTS} from "../assets/data/texts.js";
+import useTypewriter from "../hooks/useTypewriter.js";
+import GameContext from "../contexts/gameContext.jsx";
+
 function Display() {
 
-    const charsPerLine = 50;
+    const {
+        breakTextIntoLines,
+        getCharacterWidth, getCharsPerLine,
+        getEndOfLineIndexes, encodeToHTMLEntities
+    } = useTypewriter();
 
-    const breakTextIntoLines = (text, charsPerLine) => {
-        const chunks = text.split(' ');
+    const {
+        text, typedText, charsPerLine, setCharsPerLine, errors
+    } = useContext(GameContext);
 
-        const lines = [];
+    const displayContainerRef = React.createRef();
+    const scrollTargetRef = React.createRef();
+    const textToTypeDisplayRef = React.createRef();
+    const typingCursorRef = React.createRef();
+    const fakeTypingAreaRef = React.createRef();
 
-        let line = []; // temporarily store chunks for a line
-        for (const chunk of chunks) {
-            line.push(chunk);
+    // - determine the number of characters to display per line
+    // - handle window resize
+    useEffect(() => {
 
-            if (line.join(' ').length > charsPerLine) {
-                const lastInsertedChunk = line.pop();
+        /*** @type HTMLElement */
+        const textToTypeDisplayEl = textToTypeDisplayRef.current;
+        setCharsPerLine(getCharsPerLine(textToTypeDisplayEl, getCharacterWidth('W')));
 
-                lines.push(line.join(' ').padEnd(charsPerLine, '_'));
-
-                line = [lastInsertedChunk];
-            }
+        const handleWindowResize = () => {
+            setCharsPerLine(getCharsPerLine(textToTypeDisplayEl, getCharacterWidth('W')));
         }
 
-        // Add the remaining ones
-        lines.push(line.join(' ').padEnd(charsPerLine, '_'));
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
 
-        return lines;
-    };
-
-    const [text, setText] = useState('');
-    useEffect(() => {
-        const textId = Math.floor(Math.random() * (TEXTS.length - 1));
-        const text = TEXTS[textId];
-
-        setText(text);
-
-        // console.log(text);
-        console.log(
-            breakTextIntoLines(text, charsPerLine)
-                .map(e => ({
-                    e: e,
-                    l: e.length
-                }))
-        );
     }, []);
 
-    const handleTyping = (e) => {
-        e.preventDefault();
+    // Run effect when ever the text typed changes
+    useEffect(() => {
 
+        // scroll the "scrollContainer" if the text to be typed goes out of view
+        scrollTargetRef.current.scrollIntoView({behavior: "smooth", block: "end"});
 
-    }
+    }, [typedText]);
+
+    const textIntoLines = breakTextIntoLines(text, charsPerLine);
 
     return (
-        <div className={displayStyles.container}>
-            <div className={displayStyles.textToTypeDisplay}>
-                {
-                    breakTextIntoLines(text, charsPerLine)
-                        .map((line, index) => (
-                            <div key={index}>{line}</div>
-                        ))
-                }
+        <div className={displayStyles.displayContainer} ref={displayContainerRef}>
+            <div className={displayStyles.scrollContainer}>
+                <div className={displayStyles.textToTypeDisplay} ref={textToTypeDisplayRef}>
+                    {
+                        textIntoLines
+                            .map(({line}, index) => (
+                                <div key={index}>{line}</div>
+                            ))
+                    }
+                </div>
+                <div className={displayStyles.typingAreaContainer}>
+                    <div className={displayStyles.fakeTypingArea} ref={fakeTypingAreaRef}>
+                        {
+                            [...typedText].map(line => line).map((char, index) => (
+                                <span
+                                    key={index}
+                                    className={errors.includes(index) ? displayStyles.error : ''}
+                                >
+                                {
+                                    // highlight errors
+                                    errors.includes(index) && char === ' ' ? (
+                                        <span className={displayStyles.whitespace}>&nbsp;</span>
+                                    ) : encodeToHTMLEntities(char)
+                                }
+                                {
+                                    // add "end of line" if the character is at the end of line
+                                    getEndOfLineIndexes(textIntoLines).includes(index) && (
+                                        <br id={index.toString()}/>
+                                    )
+                                }
+                            </span>
+                            ))
+                        }
+                        <div className={displayStyles.typingCursor} ref={typingCursorRef}></div>
+                        <div className={displayStyles.scrollTarget} ref={scrollTargetRef}></div>
+                    </div>
+                </div>
             </div>
-            <div className={displayStyles.typingAreaContainer}>
-                <textarea
-                    className={displayStyles.typingArea}
-                    onInput={handleTyping}
-                >
-                </textarea>
-            </div>
+
         </div>
     );
 }
