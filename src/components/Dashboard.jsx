9e738ec0b07847f4665b72f1febb2bbb83ad530a
Shadow Walker <franklynpeter2006@gmail.com>
2025-02-06 01:22:27 +0100

    add typewriter dashboard to codebase

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import {useContext} from 'react';
 import dashboardStyles from '../assets/styles/dashboard.module.css';
+import GameContext from "../contexts/gameContext.jsx";
+import useTypewriter from "../hooks/useTypewriter.js";
+import {GAME_STATUS} from "../constants/GameConstants.js";
 
-function Dashboard(props) {
+function Dashboard() {
+
+    const { gameStatus, text, typedText, errors, timeSpent} = useContext(GameContext);
+
+    const { formatTime, getWordsPerMinute } = useTypewriter();
+
+    const maxProgress = 100;
+    const progress = gameStatus >= GAME_STATUS.LOADED ? (typedText.length / text.length) * 100 : 0;
+
     return (
         <div className={dashboardStyles.container}>
             <div className={dashboardStyles.progressDisplayContainer}>
-                <progress className={dashboardStyles.progressBar} value={75} max={100}>
-                    Hello
+                <progress 
+                    className={dashboardStyles.progressBar}
+                    value={progress}
+                    max={maxProgress}
+                >
+                    Sorry, we can&#39;t display the progress bar because your browser does not support it
                 </progress>
             </div>
 
             <div className={dashboardStyles.metricsDisplayContainer}>
                 <div>
                     <h4>Time Spent</h4>
-                    <div>00:00:00</div>
+                    <div>{formatTime(timeSpent)}</div>
                 </div>
 
                 <div>
                     <h4>Mistakes</h4>
-                    <div>21</div>
+                    <div>{errors.length}</div>
                 </div>
 
                 <div>
                     <h4>WPM</h4>
-                    <div>19</div>
+                    <div>{getWordsPerMinute(typedText, timeSpent, errors.length)}</div>
                 </div>
             </div>
         </div>
